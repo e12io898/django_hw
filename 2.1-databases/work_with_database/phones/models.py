6541b5f831bc3c3fc96a8b0e328ec9ca8924099a
e12io898 <e12io898@ya.ru>
2023-08-01 22:53:10 +0400

    test

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    image = models.CharField()
+    price = models.IntegerField()
+    release_date = models.DateField()
+    lte_exist = models.BooleanField()
+    slug = models.SlugField(max_length=50, db_column='name')
