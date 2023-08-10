ebf00d29b1d19e75120517db4b21d34a64f5df4e
e12io898 <e12io898@ya.ru>
2023-08-10 17:55:27 +0400

    1 task

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -74,8 +74,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PORT': '5433',
+        'PASSWORD': '3312'
     }
 }
 
