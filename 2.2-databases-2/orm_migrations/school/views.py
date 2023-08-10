26fae5a46533b9a7a22d1c9298a307e07bd53ad6
e12io898 <e12io898@ya.ru>
2023-08-10 17:57:46 +0400

    1 task

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,7 +6,7 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    object_list = Student.objects.all().grou_by('group')
+    object_list = Student.objects.all().order_by('group')
     context = {'object_list': object_list}
 
     return render(request, template, context)
