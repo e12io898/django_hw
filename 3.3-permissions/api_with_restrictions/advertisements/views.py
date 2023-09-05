5dbc2062cc9a62b229ccaf61aefa3d17e1549b69
e12io898 <e12io898@ya.ru>
2023-09-05 20:58:59 +0400

    correction

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,20 +1,12 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.throttling import AnonRateThrottle
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 from rest_framework.viewsets import ModelViewSet
 from django_filters import rest_framework as filters
 
+from .filters import AdvertisementFilter
 from .models import Advertisement
 from .permissions import IsOwnerOrReadOnly
 from .serializers import AdvertisementSerializer
-
-
-class DateFilter(filters.FilterSet):
-    created_at = filters.DateFromToRangeFilter()
-
-    class Meta:
-        model = Advertisement
-        fields = ['id', 'title', 'description',
-                  'creator', 'status', 'created_at']
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -22,14 +14,14 @@ class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     filter_backends = [filters.DjangoFilterBackend]
-    filterset_class = DateFilter
+    filterset_class = AdvertisementFilter
 
-    throttle_classes = [AnonRateThrottle]
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
             return [IsAuthenticated(), IsOwnerOrReadOnly()]
         return []
