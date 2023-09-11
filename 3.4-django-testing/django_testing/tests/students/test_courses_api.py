7a460bd707577a5bb3f62b1f174802af4e5a74de
e12io898 <e12io898@ya.ru>
2023-09-11 19:35:41 +0400

    final

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,12 +1,11 @@
 import random
-
 import pytest
 from rest_framework.test import APIClient
 from model_bakery import baker
 
 from students.models import Student, Course
 
-# Фикстуры
+# Фикстуры:
 
 @pytest.fixture
 def client():
@@ -29,19 +28,22 @@ def student_factory():
     return factory
 
 
-# Тесты
+# Тесты:
 
+# Проверка получения первого курса:
 @pytest.mark.django_db
 def test_course(client, course_factory):
     course = course_factory(_quantity=1)
+    index = course[0].id
 
-    response = client.get('/api/v1/courses/1/')
+    response = client.get(f'/api/v1/courses/{index}/')
     data = response.json()
 
     assert response.status_code == 200
-    assert data['id'] == 1
+    assert data['id'] == index
 
 
+# Проверка получения списка курсов:
 @pytest.mark.django_db
 def test_courses(client, course_factory):
     courses = course_factory(_quantity=10)
@@ -55,12 +57,70 @@ def test_courses(client, course_factory):
         assert courses[index].id == course['id']
 
 
+# Проверка фильтрации списка курсов по id:
 @pytest.mark.django_db
 def test_filter_id(client, course_factory):
-    courses = course_factory(_quantity=1)
+    courses = course_factory(_quantity=10)
+    indexes = [i.id for i in courses]
+    index = random.choice(indexes)
 
-    response = client.get('/api/v1/courses/')
+    response = client.get(f'/api/v1/courses/?id={index}')
     data = response.json()
 
     assert response.status_code == 200
+    assert index == data[0]['id']
 
+
+# Проверка фильтрации списка курсов по name:
+@pytest.mark.django_db
+def test_filter_name(client, course_factory):
+    courses = course_factory(_quantity=10)
+    names = [item.name for item in courses]
+    name = random.choice(names)
+
+    response = client.get(f'/api/v1/courses/?name={name}')
+    data = response.json()
+
+    assert response.status_code == 200
+    assert name == data[0]['name']
+
+
+# Тест успешного создания курса:
+@pytest.mark.django_db
+def test_create_course(client):
+    count = Course.objects.count()
+    data = {
+        'name': 'Python-разработчик с нуля'
+    }
+
+    response = client.post('/api/v1/courses/', data=data)
+
+    assert response.status_code == 201
+    assert Course.objects.count() == count + 1
+
+
+# Тест успешного обновления курса:
+@pytest.mark.django_db
+def test_patch_course(client, course_factory):
+    course = course_factory(_quantity=1)
+    index = course[0].id
+
+    data = {
+        'name': 'Python-разработчик с нуля'
+    }
+
+    response = client.patch(f'/api/v1/courses/{index}/', data=data)
+
+    assert response.status_code == 200
+    assert response.json()['name'] == data['name']
+
+
+# Тест успешного удаления курса:
+@pytest.mark.django_db
+def test_delete_course(client, course_factory):
+    course = course_factory(_quantity=1)
+    index = course[0].id
+
+    response = client.delete(f'/api/v1/courses/{index}/')
+
+    assert response.status_code == 204
